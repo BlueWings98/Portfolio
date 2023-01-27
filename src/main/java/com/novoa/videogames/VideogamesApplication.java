3b35520087c055f3b310c003f4a933bc9fe6446f
BlueWings98 <sebastiannovoa1998@hotmail.com>
2023-01-27 08:20:55 -0500

    Proyect created

---
+++ src/main/java/com/novoa/videogames/VideogamesApplication.java
@@ -0,0 +1,13 @@
+package com.novoa.videogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamesApplication.class, args);
+	}
+
+}
